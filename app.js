232f50a077aa2db7e4f300d8f506a136ed9e8b61
silsfra <chanyajnurak0@gmail.com>
2024-04-06 18:44:54 +0700

    Add all files of Projects

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 4000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req, res) => {
+
+    res.send('Hello World!');
+
+})
+
+app.listen(port, ()=>{
+
+    debug("Listening on port " + port);
+
+})
