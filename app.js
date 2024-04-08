be754caef2bcd70edf33acf1498c13a0e8bf6aa4
silsfra <chanyajnurak0@gmail.com>
2024-04-08 14:56:58 +0700

    onepagelove

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 4000;
 
 app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
 
